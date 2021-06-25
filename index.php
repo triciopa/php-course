ea48a34e4c4e388279b6a7d5ba5a262f26131bc4
Patricio <67854017+triciopa@users.noreply.github.com>
2021-06-25 17:28:48 -0300

    Print in HTML, echo and variables

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
-<?php
-  echo "Hello adfadf!"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP - First attempt</title>
+</head>
+<body>
+  <h1>Hello World</h1>
+
+ <?php
+    // Printing to HTML using echo
+    echo "I'm a PHP sentence";
+    echo "<hr/>";
+    echo "Second line";
+    echo "<br>";    
+ ?>
+ 
+ <?php
+    $name = 'Pato';
+    $age  = 34;
+    // echo $name;
+    echo "<h4>My name is: ".$name."</h4>";
+    echo "<h5>My age is: ".$age."</h5>";
+  ?>
+
+
+</body>
+</html>
