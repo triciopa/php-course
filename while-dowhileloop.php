19ca619fba4c2ee559907fddc86abf21bbde4cb3
Patricio <67854017+triciopa@users.noreply.github.com>
2021-06-28 12:44:24 -0300

    Do While loop

--- while-dowhileloop.php
+++ while-dowhileloop.php
@@ -4,13 +4,13 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP - While & Do-while Loops </title>
+  <title>PHP - While & Do-While Loops </title>
 </head>
 <body>
   <h1>While Loop</h1>
   <?php
     $grade = 0;
-    while($grade < 10){
+    while($grade < 5){
       echo "<p>I'm less than ten</p>";
       echo $grade;
       $grade++;
@@ -18,6 +18,15 @@
     echo "<br>";
     echo 'Exit Loop';
   ?>
-  
+
+  <h1>Do-While Loop</h1>
+  <?php
+    $grade = 10;
+
+    do{
+      echo "<p>I'm doing this while some conditions exist</p>";
+      echo $grade--;      
+    }while($grade > 5);
+  ?>
 </body>
 </html>
