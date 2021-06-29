1f51a19e0670a5241b41e4ef92b78c146c25f35a
Patricio <67854017+triciopa@users.noreply.github.com>
2021-06-29 09:15:48 -0300

    PHP includes + Header

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP - Crash course</title>
+</head>
+<body>
+  <h2>Follow each link to the page example</h2>
+  <ul>
+    <li><a href="array.php">Simple Array and Printouts</a></li>
+    <li><a href="forloop.php">Simple For Loop</a></li>
+    <li><a href="while-dowhileloop.php">While & Do While Loop</a></li>
+    <li><a href="ifstatement.php">Simple If Statement</a></li>
+    <li><a href="switchstatement.php">Simple Switch Statement</a></li>
+    <li><a href="stringmanip.php">String Manipulation</a></li>
+    <li><a href="datetime.php">Date and Time</a></li>
+    <li><a href="functions.php">Functions</a></li>
+  </ul>
--- index.php
+++ index.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP - First attempt</title>
-</head>
-<body>
-  <h2>Follow each link to the page example</h2>
-  <li><a href="array.php">Simple Array and Printouts</a></li>
-  <li><a href="forloop.php">Simple For Loop</a></li>
-  <li><a href="while-dowhileloop.php">While & Do While Loop</a></li>
-  <li><a href="ifstatement.php">Simple If Statement</a></li>
-  <li><a href="switchstatement.php">Simple Switch Statement</a></li>
-  <li><a href="stringmanip.php">String Manipulation</a></li>
-  <li><a href="datetime.php">Date and Time</a></li>
-  <li><a href="functions.php">Functions</a></li>
+<?php include 'includes/header.php' ?>
 
-  <h1>Hello World</h1>
+  <h1>Hello PHP</h1>
 
  <?php
     // Printing to HTML using echo
