8d0969e108074f832db20a2b021e143c59f64bd3
Patricio <67854017+triciopa@users.noreply.github.com>
2021-06-29 09:49:43 -0300

    Bootstrap installed

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,11 @@
-<?php
+  <?php
   echo "<p>Copywright &copy;1987-". date("Y") . " PHP Masterclass</p>";
   ?>
-
+  <!-- JavaScript Bundle with Popper -->
+  <script 
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+  integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+  crossorigin="anonymous">
+  </script>
 </body>
 </html>
