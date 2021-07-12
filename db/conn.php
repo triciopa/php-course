9f9a18d7bbef912214d05b8aed70c2d8c68b0582
Patricio <patriciogatti@gmail.com>
2021-07-12 14:50:26 -0300

    Test production / Users

--- db/conn.php
+++ db/conn.php
@@ -1,17 +1,17 @@
 <?php
   // Development Connection
-  $host = '127.0.0.1';
-  $db = 'attendance_db';
-  $user = 'root';
-  $pass = '';
-  $charset = 'utf8mb4';
+  // $host = '127.0.0.1';
+  // $db = 'attendance_db';
+  // $user = 'root';
+  // $pass = '';
+  // $charset = 'utf8mb4';
   
   //Remote Database Connection
-  // $host = 'remotemysql.com';
-  // $db = 'JHPJ9kjrwG';
-  // $user = 'JHPJ9kjrwG';
-  // $pass = 'lAo0TaBKo7';
-  // $charset = 'utf8mb4';
+  $host = 'remotemysql.com';
+  $db = 'JHPJ9kjrwG';
+  $user = 'JHPJ9kjrwG';
+  $pass = 'lAo0TaBKo7';
+  $charset = 'utf8mb4';
   
 
   $dsn = "mysql:host=$host;dbname=$db;charset=$charset" ;
