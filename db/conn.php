c7164fd5c13727ba4c8ea9705f09f5e16874318d
Patricio <patriciogatti@gmail.com>
2021-07-12 13:44:21 -0300

    DB remote connection

--- db/conn.php
+++ db/conn.php
@@ -1,9 +1,18 @@
 <?php
-  $host = '127.0.0.1';
-  $db = 'attendance_db';
-  $user = 'root';
-  $pass = '';
+  // Development Connection
+  // $host = '127.0.0.1';
+  // $db = 'attendance_db';
+  // $user = 'root';
+  // $pass = '';
+  // $charset = 'utf8mb4';
+  
+  //Remote Database Connection
+  $host = 'remotemysql.com';
+  $db = 'JHPJ9kjrwG';
+  $user = 'JHPJ9kjrwG';
+  $pass = 'lAo0TaBKo7';
   $charset = 'utf8mb4';
+  
 
   $dsn = "mysql:host=$host;dbname=$db;charset=$charset" ;
 
